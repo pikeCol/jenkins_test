cd7cf9fc8cdb5a83f3a5ef490bbd091a58e30741
林镇方 <spx-linzf@supaur.com>
2019-05-06 18:11:05 +0800

    change port

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 // Constants
-const PORT = 8086;
-const HOST = '0.0.0.0';
+const PORT = 9999
+const HOST = '0.0.0.0'
 
 // App
-const app = express();
-console.log(path.join(__dirname,'./index.html'))
+const app = express()
+console.log(path.join(__dirname, './index.html'))
 app.get('/sub.html', (req, res) => {
-  res.sendFile(path.join(__dirname,'./sub.html'))
-});
+  res.sendFile(path.join(__dirname, './sub.html'))
+})
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'./index.html'))
-});
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+  res.sendFile(path.join(__dirname, './index.html'))
+})
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
